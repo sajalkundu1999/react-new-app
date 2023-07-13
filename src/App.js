5db90abff12f46b7a5575a76ed02412c75637f7a
sajalkundu199 <sajalkundu199@gmail.com>
2023-07-13 15:54:26 +0530

    15:54

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> my name is sajal kundu
+          Edit <code>src/App.js</code> my name is sajal kundu & This is my first project.
         </p>
         <a
           className="App-link"
