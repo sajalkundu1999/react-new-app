43795190d3d1ab7eb1e87916134d270c12caa61e
sajalkundu199 <sajalkundu199@gmail.com>
2023-07-13 14:07:07 +0530

    14:07

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>The content on the website and/or Platform is for informational and educational purposes only. The user of this website and/or Platform (User) should not construe any such information as legal, investment, tax, financial or any other advice.
+          Edit <code>src/App.js</code> my name is sajal kundu
         </p>
         <a
           className="App-link"
