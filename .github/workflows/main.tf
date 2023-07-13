8663ac5aea26b913aefa18d77a7f3adaebd99bc1
sajalkundu199 <sajalkundu199@gmail.com>
2023-07-13 10:30:30 +0530

    10:30

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+resource "aws_instance" "react-new-app" {
+  ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
 }
